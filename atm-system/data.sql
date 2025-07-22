07c5dd6812a0ddfde99754f48e5ab5bea94fdbf1
Aolakije <TYC@AolakijeMacBook.localdomain>
2025-07-22 13:37:23 +0200

    updated tables to use new entries

--- atm-system/data.sql
+++ atm-system/data.sql
@@ -6,15 +6,22 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 -- Create accounts table
+
+
+-- DROP TABLE IF EXISTS accounts;
 CREATE TABLE IF NOT EXISTS accounts (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
-    name TEXT NOT NULL,
-    account_id INTEGER NOT NULL,
-    date TEXT NOT NULL,
+    username TEXT NOT NULL,
+    account_id INTEGER NOT NULL UNIQUE,
+    creation_date TEXT NOT NULL,
     country TEXT NOT NULL,
-    phone TEXT NOT NULL,
-    balance REAL NOT NULL,
+    phone_number TEXT NOT NULL,
+    balance REAL NOT NULL DEFAULT 0.0,
     account_type TEXT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+SELECT * FROM users;
+
+SELECT * FROM accounts;
