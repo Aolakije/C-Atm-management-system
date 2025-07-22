c49a8f026fd0ca13a8ee7995de30b387cf1c672f
Aolakije <TYC@AolakijeMacBook.localdomain>
2025-07-22 10:11:28 +0200

    updted the auth.c to call funtions to use sqlite database for login and register users

--- atm-system/src/auth.c
+++ atm-system/src/auth.c
@@ -1,10 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <termios.h>
+#include <sqlite3.h>
 #include "header.h"
+#include "db.h"
 
-char *USERS = "./data/users.txt";
-
-void loginMenu(char a[50], char pass[50])
-{
+void loginMenu(char a[50], char pass[50]) {
     struct termios oflags, nflags;
 
     system("clear");
@@ -17,81 +19,69 @@ void loginMenu(char a[50], char pass[50])
     nflags.c_lflag &= ~ECHO;
     nflags.c_lflag |= ECHONL;
 
-    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
-    {
+    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0) {
         perror("tcsetattr");
-        return exit(1);
+        exit(1);
     }
+
     printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
     scanf("%s", pass);
 
     // restore terminal
-    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
-    {
+    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0) {
         perror("tcsetattr");
-        return exit(1);
-    }
-};
-
-const char *getPassword(struct User u)
-{
-    FILE *fp;
-    static struct User userChecker;  // Made static to persist after function returns
-
-    if ((fp = fopen("./data/users.txt", "r")) == NULL)
-    {
-        printf("Error! opening file");
         exit(1);
     }
+}
 
-    while (fscanf(fp, "%d %s %s", &userChecker.id, userChecker.name, userChecker.password) != EOF)
-    {
-        if (strcmp(userChecker.name, u.name) == 0)
-        {
-            fclose(fp);
-            return userChecker.password;
-        }
+const char *getPassword(struct User u) {
+    static char foundPassword[50];
+    sqlite3_stmt *stmt;
+    const char *sql = "SELECT password FROM users WHERE name = ?";
+
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) != SQLITE_OK) {
+        fprintf(stderr, "SQL prepare error: %s\n", sqlite3_errmsg(db));
+        return "error";
     }
 
-    fclose(fp);
+    sqlite3_bind_text(stmt, 1, u.name, -1, SQLITE_STATIC);
+
+    int rc = sqlite3_step(stmt);
+    if (rc == SQLITE_ROW) {
+        strncpy(foundPassword, (const char *)sqlite3_column_text(stmt, 0), sizeof(foundPassword));
+        sqlite3_finalize(stmt);
+        return foundPassword;
+    }
+
+    sqlite3_finalize(stmt);
     return "no user found";
 }
 
-void registerMenu(char a[50], char pass[50])
-{
+void registerMenu(char a[50], char pass[50]) {
     struct termios oflags, nflags;
-    FILE *fp;
-    struct User userChecker;
+    sqlite3_stmt *stmt;
     int userExists = 0;
-    int nextId = 1;
 
     system("clear");
     printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Registration:");
     
-    // Get username
     printf("\n\n\t\t\t\tEnter username: ");
     scanf("%s", a);
 
-    // Check if user already exists and get next ID
-    if ((fp = fopen("./data/users.txt", "r")) != NULL)
-    {
-        while (fscanf(fp, "%d %s %s", &userChecker.id, userChecker.name, userChecker.password) != EOF)
-        {
-            if (strcmp(userChecker.name, a) == 0)
-            {
-                userExists = 1;
-                break;
-            }
-            if (userChecker.id >= nextId)
-            {
-                nextId = userChecker.id + 1;
-            }
-        }
-        fclose(fp);
+    // Check if user already exists
+    const char *checkSql = "SELECT COUNT(*) FROM users WHERE name = ?";
+    if (sqlite3_prepare_v2(db, checkSql, -1, &stmt, NULL) != SQLITE_OK) {
+        fprintf(stderr, "SQL prepare error: %s\n", sqlite3_errmsg(db));
+        return;
     }
+    sqlite3_bind_text(stmt, 1, a, -1, SQLITE_STATIC);
+    if (sqlite3_step(stmt) == SQLITE_ROW) {
+        int count = sqlite3_column_int(stmt, 0);
+        if (count > 0) userExists = 1;
+    }
+    sqlite3_finalize(stmt);
 
-    if (userExists)
-    {
+    if (userExists) {
         printf("\n\n\t\t\t\tUser already exists! Please choose a different username.\n");
         printf("\n\n\t\t\t\tPress Enter to continue...");
         getchar();
@@ -104,9 +94,7 @@ void registerMenu(char a[50], char pass[50])
     nflags = oflags;
     nflags.c_lflag &= ~ECHO;
     nflags.c_lflag |= ECHONL;
-
-    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
-    {
+    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0) {
         perror("tcsetattr");
         return;
     }
@@ -115,24 +103,28 @@ void registerMenu(char a[50], char pass[50])
     scanf("%s", pass);
 
     // Restore terminal
-    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
-    {
+    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0) {
         perror("tcsetattr");
         return;
     }
 
-    // Save new user to file
-    fp = fopen("./data/users.txt", "a");
-    if (fp == NULL)
-    {
-        printf("Error opening file for writing!\n");
+    // Insert new user
+    const char *insertSql = "INSERT INTO users (name, password) VALUES (?, ?)";
+    if (sqlite3_prepare_v2(db, insertSql, -1, &stmt, NULL) != SQLITE_OK) {
+        fprintf(stderr, "SQL prepare error: %s\n", sqlite3_errmsg(db));
         return;
     }
 
-    fprintf(fp, "%d %s %s\n", nextId, a, pass);
-    fclose(fp);
+    sqlite3_bind_text(stmt, 1, a, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, pass, -1, SQLITE_STATIC);
 
-    printf("\n\n\t\t\t\tRegistration successful! You can now login.\n");
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        fprintf(stderr, "SQL insert error: %s\n", sqlite3_errmsg(db));
+    } else {
+        printf("\n\n\t\t\t\tRegistration successful! You can now login.\n");
+    }
+
+    sqlite3_finalize(stmt);
     printf("\n\n\t\t\t\tPress Enter to continue...");
     getchar();
     getchar();
